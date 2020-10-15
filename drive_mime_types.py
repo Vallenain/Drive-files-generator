a301d99c8c684760deab5b5a961b253cdef9b436
Vallenain <vallenain@free.fr>
2020-10-15 21:39:07 +0200

    Initial commit

---
+++ drive_mime_types.py
@@ -0,0 +1,9 @@
+DRIVE_MIME_TYPES = {
+    'docs':	'application/vnd.google-apps.document',
+    'drawings': 'application/vnd.google-apps.drawing',
+    'forms': 'application/vnd.google-apps.form',
+    'slides': 'application/vnd.google-apps.presentation',
+    'scripts': 'application/vnd.google-apps.script',
+    'sites': 'application/vnd.google-apps.site',
+    'sheets': 'application/vnd.google-apps.spreadsheet'
+}
