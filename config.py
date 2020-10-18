18b31bfcabeb87408983972f0084d6491f97682a
Vallenain <vallenain@free.fr>
2020-10-18 14:21:27 +0200

    added tests

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 CONFIG = {
-    'CLIENT_SECRET_PATH': 'credentials/client_secret.json',
-    'CREDS_PATH': 'credentials/credentials.json',
-    'SCOPES': ['https://www.googleapis.com/auth/userinfo.email',
-               'https://www.googleapis.com/auth/userinfo.profile',
-               'https://www.googleapis.com/auth/drive',
-               'openid'],
-    'DRIVE_FILES_PATH': 'inputs/my_example_file.json'
+    "CLIENT_SECRET_PATH": "credentials/client_secret.json",
+    "CREDS_PATH": "credentials/credentials.json",
+    "SCOPES": [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/drive",
+        "openid",
+    ],
+    "DRIVE_FILES_PATH": "inputs/my_example_file.json",
 }
---
+++ test_main.py
@@ -0,0 +1,78 @@
+import pytest
+
+import main
+from config import CONFIG
+
+
+def test_read_json_with_none():
+    with pytest.raises(TypeError):
+        main.read_json()
+    with pytest.raises(FileNotFoundError):
+        main.read_json("")
+
+
+def test_build_drive_service():
+    main.build_drive_service()
+    assert main.drive_service is not None
+
+
+def test_make_creds_from_file():
+    with pytest.raises(TypeError):
+        main.make_creds_from_file()
+    with pytest.raises(FileNotFoundError):
+        main.make_creds_from_file("")
+    assert main.make_creds_from_file(CONFIG["CREDS_PATH"]) is not None
+
+
+def test_export_cred_to_file(tmpdir):
+    creds = main.make_creds_from_file(CONFIG["CREDS_PATH"])
+    creds_tmp_path = "{}/creds.json".format(tmpdir)
+    with pytest.raises(FileNotFoundError):
+        main.export_cred_to_file("", creds)
+    with pytest.raises(AttributeError):
+        main.export_cred_to_file(creds_tmp_path, None)
+    main.export_cred_to_file(creds_tmp_path, creds)
+    assert main.make_creds_from_file(creds_tmp_path) is not None
+
+
+PROCESS_LEVEL_INPUTS = [
+    ({}, None),
+    ({"sblah": "nothing"}, None),
+    ({"folders": {}}, None),
+    ({"folders": []}, main.JSONTreeError),
+    ({"folders": {"multiple": {}, "folders": {}}}, None),
+    ({"folders": {"my_folder": {"docs": 1}}}, None),
+]
+
+
+@pytest.mark.parametrize("tree, exception", PROCESS_LEVEL_INPUTS)
+def test_process_level(mocker, tree, exception):
+    parent_folder_id = "A_FAKE_ONE"
+    mocker.patch("main.create_file", return_value="A_FAKE_ID")
+
+    if exception:
+        with pytest.raises(exception):
+            main.process_level(parent_folder_id, tree)
+    else:
+        main.process_level(parent_folder_id, tree)
+
+
+CREATE_DRIVE_FILES_INPUT = [
+    ({}, main.JSONTreeError),
+    ({"sblah": {}}, None),
+    ({"sblah": {}, "sblah2": {}}, main.JSONTreeError),
+    ({"my_drive": {"docs": 1}}, None),
+    ([], main.JSONTreeError),
+    ([{"docs": 1}], main.JSONTreeError),
+]
+
+
+@pytest.mark.parametrize("tree, exception", CREATE_DRIVE_FILES_INPUT)
+def test_create_drive_files(mocker, tree, exception):
+    mocker.patch("main.create_file", return_value="A_FAKE_ID")
+
+    if exception:
+        with pytest.raises(exception):
+            main.create_drive_files(tree)
+    else:
+        main.create_drive_files(tree)
