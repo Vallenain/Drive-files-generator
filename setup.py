6159d04b85d4c71159587020d85d6d203dd108a6
Vallenain <vallenain@free.fr>
2020-10-31 12:34:56 +0100

    renamed main + improved setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="drive-files-gen",
-    version="0.0.1",
+    version="0.0.2",
     author="Val Le Nain",
     author_email="vallenain@gmail.com",
     description="Tool to generate files on Google Drive from a JSON file",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Vallenain/Drive-files-generator",
-    packages=setuptools.find_packages(),
+    tests_require=["pytest"],
+    py_modules=["drive_files_gen"],
+    keywords="google-drive JSON automation",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Utilities"
     ],
     python_requires='>=3.6',
 )
